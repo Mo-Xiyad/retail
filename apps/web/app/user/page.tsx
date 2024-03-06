43f459fb1dbd154687b2821d0d8f229749e2c26a
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-03-06 15:36:37 +0100

    additional route

---
+++ apps/web/app/user/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { Button } from '@repo/ui/src/index';
+
+function Gradient({
+  conic,
+  className,
+  small
+}: {
+  small?: boolean;
+  conic?: boolean;
+  className?: string;
+}): JSX.Element {
+  return (
+    <span
+      className={`absolute mix-blend-normal will-change-[filter] rounded-[100%] ${
+        small ? 'blur-[32px]' : 'blur-[75px]'
+      } ${conic ? 'bg-glow-conic' : ''} ${className}`}
+    />
+  );
+}
+export default function Web() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gradient-to-br from-[#a79494] to-[#fcfbfb]">
+      <h1>User</h1>
+      <Button onClick={() => console.log('Pressed!')} text="User!!!" />
+      <div className="relative flex place-items-center ">
+        <p className="text-4xl font-bold text-center">This is user page</p>
+      </div>
+    </div>
+  );
+}
