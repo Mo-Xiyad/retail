9c80609bdf158ab6db5227d46fe941ff708eaeae
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-03-06 17:43:36 +0100

    database

---
+++ packages/database/tsup.config.ts
@@ -0,0 +1,40 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { defineConfig, Options } from 'tsup';
+
+export default defineConfig((options: Options) => ({
+  dts: true,
+  entry: ['src/schema/**/*.{ts,tsx}'],
+  clean: true,
+  bundle: true,
+  esbuildPlugins: [
+    {
+      name: 'add-js',
+      setup(build) {
+        build.onResolve({ filter: /.*/ }, async (args) => {
+          if (args.importer && !args.path.endsWith('.js')) {
+            const isFile = await fs
+              .lstat(
+                path.resolve(path.dirname(args.importer), args.path + '.ts')
+              )
+              .then((stat) => stat.isFile())
+              .catch(() => false);
+            return {
+              path:
+                args.path +
+                (args.path.startsWith('.')
+                  ? isFile
+                    ? '.js'
+                    : '/index.js'
+                  : ''),
+              external: true
+            };
+          }
+        });
+      }
+    }
+  ],
+  tsconfig: 'tsconfig.json',
+  format: ['cjs', 'esm'],
+  ...options
+}));
