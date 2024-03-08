b661f0c193e37f884ff8d2ab40e2e3743038913e
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-03-08 14:21:09 +0100

    wrap the provider

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,13 +1,16 @@
 import '@repo/ui/src/styles.css';
 import '../styles/global.css';
+import { ClientProviders } from './useProvider';
 export default function RootLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClientProviders>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClientProviders>
   );
 }
