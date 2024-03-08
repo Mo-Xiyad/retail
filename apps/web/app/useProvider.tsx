6889b2e8838684022c90dc3dc7db56754ca3e83b
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-03-08 14:13:51 +0100

    add provider

---
+++ apps/web/app/useProvider.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
+import { QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import { trpc, useTrpcClient } from './_trpc/client';
+
+export function ClientProviders(props: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark
+      }}
+    >
+      <InnerProvider>{props.children}</InnerProvider>
+    </ClerkProvider>
+  );
+}
+const InnerProvider = (props: { children: React.ReactNode }) => {
+  const { trpcClient, queryClient } = useTrpcClient();
+  return (
+    <trpc.Provider queryClient={queryClient} client={trpcClient}>
+      <QueryClientProvider client={queryClient}>
+        {props.children}
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+};
