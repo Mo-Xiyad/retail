706d22f2fa1cf72b61c21d3c4f9bbb4895593a2c
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-03-11 23:43:54 +0100

    add ui lib

--- apps/web/src/app/(root)/page.tsx
+++ apps/web/src/app/(root)/page.tsx
@@ -1,6 +1,15 @@
 'use client';
 
-import { Button } from '@repo/ui/src/index';
+import { Badge } from '@/components/ui/badge';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
 import { trpc } from '../_trpc/client';
 
 export default function Web() {
@@ -10,12 +19,27 @@ export default function Web() {
   }
   console.log(data);
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gradient-to-br from-[#a79494] to-[#fcfbfb]">
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-cyan-100">
       <h1>Web</h1>
-      <Button onClick={() => console.log('Pressed!')} text="Boop" />
+      {/* <Button onClick={() => console.log('Pressed!')} text="Boop" /> */}
+      <Badge variant="default">Badge</Badge>
+      <Label htmlFor="email">Your email address</Label>
+
       <div className="relative flex place-items-center ">
         <p className="text-4xl font-bold text-center">Hello, world!</p>
       </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
